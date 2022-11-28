421767e077c4d8a95b031df571282084578326fa
Len <wangl_msg@163.com>
2022-11-28 15:58:26 +0800

    调整样式

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   const collapsed = useAppSelector(selectCollapsed);
   return (
     <div className="logo">
-      <img style={{ width: 48 }} src="/logo64.png" />
+      <img style={{ width: 43 }} src="/logo64.png" />
       <div style={{ width: !collapsed ? 141 : 0, transition: 'width 0.3s' }}>
         <h1 className="logo_text">Fine Admin</h1>
       </div>
